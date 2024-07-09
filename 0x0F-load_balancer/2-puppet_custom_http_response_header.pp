0695c4a1d269eef682eed4db426834ceea2c9733
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-09 10:57:31 +0200

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,25 @@
-# Ensures the package 'nginx' is installed
+# Ensures the package is up to date
+exec { 'update_packages':
+  command => '/usr/bin/apt-get update',
+}
+
+# Installs Nginx
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['update_packages'],
 }
 
-# Ensures that the Nginx service is running and enabled at boot
+# Creates a custom Nginx configuration file
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => present,
+  content => "add_header X-Served-By ${::hostname};",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensures Nginx service is running and enabled
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx'],
-}
-
-# The custom header configuration directly in the Puppet manifest definded
-class custom_nginx_header ($hostname) {
-  $custom_header_content = @(END)
-add_header X-Served-By ${hostname};
-END
-
-  # The custom header configuration file created
-  file { '/etc/nginx/conf.d/custom_header.conf':
-    ensure  => file,
-    content => $custom_header_content,
-    notify  => Service['nginx'],
-  }
-}
-
-# The custom_nginx_header class with hostname fact included
-class { 'custom_nginx_header':
-  hostname => $facts['hostname'],
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/conf.d/custom_header.conf'],
 }
